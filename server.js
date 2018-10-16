39b023d467c6906e98b82abfcf3bfaa8c2043b4a
georgealexnadruiancu <iancugeorgealexandru@gmail.com>
2018-10-16 11:30:22 +0300

    signUp to mongoDB

--- server.js
+++ server.js
@@ -11,9 +11,28 @@ mongo.connect('mongodb://localhost/hotelsLogin', function(err, db){
     console.log("connected to hotelsLogin");
 
     client.on('connection', function(socket){
-        let login = db.collection('login');
+        let login = db.collection('users');
         socket.on("submit", function(data){
-            
+            let user = data.name;
+            let pwd = data.password;
+            if( user == "" || pwd == ""){
+                console.log("on empty fileds");
+            }else{
+                //HASH THE PASSWORD
+                bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
+                    if(err) throw err;
+                    bcrypt.hash(pwd, salt, function(err, hash){
+                        if(err) throw err;
+
+                        pwd = hash;
+
+                        login.insert({username: user, password: pwd}, function(){
+                            console.log("A new user has signed up" + user);
+                        })
+                        console.log(login.find())
+                    })
+                })
+            }
         })
     })
 })
